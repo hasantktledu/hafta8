a68cd3b0fa77e9399df077309484450646ac2914
Hasan Tokatlı Edu <hasantktledu@gmail.com>
2024-03-03 16:06:50 +0300

    feat: Darkmode özelliği geldi.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Repoyu önce githubda oluşturdum.
+    </div>
+  );
+}
+
+export default App;
