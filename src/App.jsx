6f1e12dce3486831d4d93e050d6f4a5c443ab34d
Hasan Tokatlı Edu <hasantktledu@gmail.com>
2024-03-03 19:51:23 +0300

    feat: State örneği yapıldı.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function App() {
+
+  const [ad, adGuncelle] = useState("Okan")
+
+  console.log("Canavar monte edilmek üzere..")
+
+
   return (
     <div>
-      Merhaba!
+      Merhaba! {ad}
+
+      <button onClick={ ()=> {  adGuncelle("Buket")   } }>Adı Değiştir</button>
+      
     </div>
   );
+
 }
 
 export default App;
